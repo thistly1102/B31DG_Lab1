b35bb4ec263e929e706596eaf8a3e6f8a7ab0c2d
thistly1102 <124710077+thistly1102@users.noreply.github.com>
2023-02-08 15:31:08 +0000

    Update main.c

--- main.c
+++ main.c
@@ -3,50 +3,67 @@
 #define SWITCH1 22
 #define SWITCH2 23
 
-int a = 100;
-int b = 100;
+int a = 300;
+int b = 1200;
 int c = 16;
-int d = 1500;
+int d = 5500;
 int mode = 4;
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  //Define Pins
   pinMode(LEDA, OUTPUT);
   pinMode(LEDB, OUTPUT);
   pinMode(SWITCH1, INPUT_PULLUP);
   pinMode(SWITCH2, INPUT_PULLUP);
+  //Constants
+  int pulsecap = c;
 }
 
 void loop() {
+  //Signal B
   digitalWrite(LEDB, HIGH);
   delay(50);
   digitalWrite(LEDB, LOW);
-  int pulsewidth = a;
-  int pulsewidthlow = b;
-  int pulsecap = c;
-  int blockdelay = d;
-  for(int i = 1; i == pulsecap; i++)
+
+//Signal A
+//Reset Pulse Width to a
+int pulsewidth = a;
+//Iterate waveform 'c' number of times
+  for(int i = 1; i <= pulsecap; i++)
   {
+    int pulsewidthlow = b;
+    int blockdelay = d;
+    //Call checkSwitch Functions
+    checkSwitch1();
+    checkSwitch2();
+    //Pulse LEDA
     digitalWrite(LEDA, HIGH);
     delay(pulsewidth);
     digitalWrite(LEDA, LOW);
     delay(pulsewidthlow);
+    //Increase pulsewidth
     pulsewidth = pulsewidth + 50;
 
   }
   delay(blockdelay);
+}
 
-  /*
-  if(digitalRead(SWITCH1 == 1))
+void checkSwitch1() 
+{
+  if(SWITCH1 == HIGH)
   {
-    digitalWrite(LEDA, HIGH);
+    delay(1000);
   }
-  else
+}
+
+void checkSwitch2() 
+{
+  if(SWITCH2 == HIGH)
   {
-    digitalWrite(LEDA, LOW);
+    pulsewidthlow = b/2;
+    blockdelay = d/2;
   }
-  delay(10); // this speeds up the simulation
-  */
 }
