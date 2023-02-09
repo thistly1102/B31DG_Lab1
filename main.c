0c699ad4d3e15235d2d4ea34956b53acbb55c977
thistly1102 <124710077+thistly1102@users.noreply.github.com>
2023-02-09 17:29:53 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,69 +1,64 @@
-#define LEDA 21
+#define LEDA 18
 #define LEDB 19
-#define SWITCH1 22
-#define SWITCH2 23
+#define SWITCH1 1
+#define SWITCH2 2
 
-int a = 300;
+const int a = 300;
+const int c = 16;
+const int increment = 50;
 int b = 1200;
-int c = 16;
 int d = 5500;
-int mode = 4;
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   //Define Pins
   pinMode(LEDA, OUTPUT);
   pinMode(LEDB, OUTPUT);
   pinMode(SWITCH1, INPUT_PULLUP);
   pinMode(SWITCH2, INPUT_PULLUP);
-  //Constants
-  int pulsecap = c;
 }
 
 void loop() {
-  //Signal B
-  digitalWrite(LEDB, HIGH);
-  delay(50);
-  digitalWrite(LEDB, LOW);
+  b = 1200;
+  d = 5500;
+
+//checkSwitch1();
+//checkSwitch2();
+//Signal B
+digitalWrite(LEDB, HIGH);
+delay(50);
+digitalWrite(LEDB, LOW);
 
 //Signal A
-//Reset Pulse Width to a
-int pulsewidth = a;
 //Iterate waveform 'c' number of times
-  for(int i = 1; i <= pulsecap; i++)
+  for(int i = 1; i <= c; i++)
   {
-    int pulsewidthlow = b;
-    int blockdelay = d;
-    //Call checkSwitch Functions
-    checkSwitch1();
-    checkSwitch2();
     //Pulse LEDA
     digitalWrite(LEDA, HIGH);
-    delay(pulsewidth);
+    delay(a + increment * i);
     digitalWrite(LEDA, LOW);
-    delay(pulsewidthlow);
-    //Increase pulsewidth
-    pulsewidth = pulsewidth + 50;
-
+    delay(b);
   }
-  delay(blockdelay);
+  //Delay between end of waveforms and start of next
+  delay(d);
 }
 
+//Check if switch1 is ON and prevent operation of system
 void checkSwitch1() 
 {
-  if(SWITCH1 == HIGH)
+  if(digitalRead(SWITCH1) == HIGH)
   {
-    delay(1000);
+    delay(5000);
   }
 }
 
-void checkSwitch2() 
+//Check if switch2 is on and alter pulsewidthlow and blockdelay variables
+int checkSwitch2() 
 {
-  if(SWITCH2 == HIGH)
+  if(digitalRead(SWITCH2) == HIGH)
   {
-    pulsewidthlow = b/2;
-    blockdelay = d/2;
+    b = 1200/2;
+    d = 5500/2;
   }
 }
