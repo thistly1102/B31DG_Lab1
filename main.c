259cb88f80461072c7ec94be94906dda3597c7b5
thistly1102 <124710077+thistly1102@users.noreply.github.com>
2023-02-14 15:30:04 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,64 +1,84 @@
+//Define LED pin numbers
 #define LEDA 18
 #define LEDB 19
+//Define Switch pin numbers
 #define SWITCH1 1
 #define SWITCH2 2
 
-const int a = 300;
-const int c = 16;
-const int increment = 50;
-int b = 1200;
-int d = 5500;
+const int a = 300;  //Width of 1st pulse
+const int c = 16;   //Number of pulses in a block
+const int increment = 50; //Pulse time increment per pulse  
+int b = 1200; //Width of space between pulses
+int d = 5500; //Space between Pulse Blocks
 
 
 void setup() {
   Serial.begin(115200);
-  //Define Pins
+  //Define LED pins
   pinMode(LEDA, OUTPUT);
   pinMode(LEDB, OUTPUT);
+  //Define Switch Pins
   pinMode(SWITCH1, INPUT_PULLUP);
   pinMode(SWITCH2, INPUT_PULLUP);
 }
 
 void loop() {
-  b = 1200;
-  d = 5500;
+//Call CheckSwitch functions to check if switches are HIGH 
+checkSwitch1();
+checkSwitch2();
 
-//checkSwitch1();
-//checkSwitch2();
-//Signal B
-digitalWrite(LEDB, HIGH);
+//Signal B Waveform
+digitalWrite(LEDB, HIGH); //Set LEDB to HIGH
 delay(50);
-digitalWrite(LEDB, LOW);
+digitalWrite(LEDB, LOW);  //Set LEDB to LOW
 
-//Signal A
-//Iterate waveform 'c' number of times
+//Signal A Waveform
+//Iterate Signal A waveform 'c = 16' number of times
   for(int i = 1; i <= c; i++)
   {
     //Pulse LEDA
-    digitalWrite(LEDA, HIGH);
-    delay(a + increment * i);
-    digitalWrite(LEDA, LOW);
-    delay(b);
+    digitalWrite(LEDA, HIGH); //Set LEDA to HIGH
+    delay(a + increment * i); //Delay by 'a' plus the incremental delay
+    digitalWrite(LEDA, LOW);  //Set LEDA to LOW
+    delay(b); //Delay by 'b'
   }
-  //Delay between end of waveforms and start of next
+  //Delay between end of Signal A waveform and start of Signal B Waveform
   delay(d);
 }
 
-//Check if switch1 is ON and prevent operation of system
+//Check if SWITCH1 is HIGH
 void checkSwitch1() 
 {
+  int stopsystem = 0;
+  //If SWITCH1 is HIGH, Stop operation of Signal A and Signal B waveforms
   if(digitalRead(SWITCH1) == HIGH)
   {
-    delay(5000);
+    stopsystem = 1;
+  }
+  //Maintain delay of operation while SWITCH1 remains HIGH
+  while(stopsystem == 1)
+  {
+    delay(1000);
+    if(digitalRead(SWITCH1) == LOW)
+    {
+      stopsystem = 0;
+    }
   }
 }
 
-//Check if switch2 is on and alter pulsewidthlow and blockdelay variables
-int checkSwitch2() 
+//Check if SWITCH2 is HIGH
+void checkSwitch2() 
 {
+  //If SWITCH2 is HIGH, half 'b' and 'd' delay variables for operation of the signal waveforms
   if(digitalRead(SWITCH2) == HIGH)
   {
     b = 1200/2;
     d = 5500/2;
+  }
+  //If SWITCH2 is LOW return b and d delay variables to original value
+  else
+  {
+    b = 1200;
+    d = 5500;
   }
 }
